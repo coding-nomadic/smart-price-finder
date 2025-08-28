ffafd36288044863647dafc6cd21dfc1b4ff1639
Tenzin Dawa <tnzngdw@gmail.com>
2025-08-28 06:53:45 -0400

    Update ProductPriceApplication.java

--- src/main/java/com/example/product/price/ProductPriceApplication.java
+++ src/main/java/com/example/product/price/ProductPriceApplication.java
@@ -2,10 +2,11 @@ package com.example.product.price;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ProductPriceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ProductPriceApplication.class, args);
 	}
